ed92bd8a4c029627db0fccd881ee1d19cdfeba7c
jambottle <jaewon.walks@gmail.com>
2022-07-08 00:30:37 +0900

    feat: Use CSS Module for scoping class names locally by default

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Button from './Button';
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1 className={styles.title}>Hello, world!</h1>
       <Button text={'Continue'} />
     </div>
   );
