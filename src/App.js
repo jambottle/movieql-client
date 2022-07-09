0a2a55fdddeaa7fada2ed0842fc4371b509e90e0
jambottle <jaewon.walks@gmail.com>
2022-07-10 00:10:37 +0900

    feat: Implement To-Do List app using useState hook and list rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import { useState } from 'react';
+
 import Button from './Button';
 import styles from './App.module.css';
 
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState([]);
+
+  const handleChange = event => setToDo(event.target.value);
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (toDo === '') return;
+    else {
+      setToDoList([toDo, ...toDoList]);
+      setToDo('');
+    }
+  };
+
   return (
-    <div>
-      <h1 className={styles.title}>Hello, world!</h1>
-      <Button text={'Continue'} />
-    </div>
+    <>
+      <h1 className={styles.title}>To-Do List ({toDoList.length})</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your To-Do..."
+        />
+        <Button text={'Add To-Do'} />
+      </form>
+
+      <hr />
+
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
