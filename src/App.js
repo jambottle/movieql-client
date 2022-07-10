b053113ec6ccdf7fe11b4f7424832d420c35291c
jambottle <jaewon.walks@gmail.com>
2022-07-10 09:30:09 +0900

    rename: Structure To-Do List app into pages and components folder

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
-import { useState } from 'react';
-
-import Button from './Button';
-import styles from './App.module.css';
+import ToDoList from './pages/ToDoList/ToDoList';
 
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDoList, setToDoList] = useState([]);
-
-  const handleChange = event => setToDo(event.target.value);
-  const handleSubmit = event => {
-    event.preventDefault();
-
-    if (toDo === '') return;
-    else {
-      setToDoList([toDo, ...toDoList]);
-      setToDo('');
-    }
-  };
-
-  return (
-    <>
-      <h1 className={styles.title}>To-Do List ({toDoList.length})</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          onChange={handleChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your To-Do..."
-        />
-        <Button text={'Add To-Do'} />
-      </form>
-
-      <hr />
-
-      <ul>
-        {toDoList.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </>
-  );
+  return <ToDoList />;
 }
 
 export default App;
---
+++ src/pages/ToDoList/ToDoList.js
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+import Button from '../../components/ToDoList/Button';
+import styles from './ToDoList.module.css';
+
+function ToDoList() {
+  const [toDo, setToDo] = useState('');
+  const [toDoList, setToDoList] = useState([]);
+
+  const handleChange = event => setToDo(event.target.value);
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if (toDo === '') return;
+    else {
+      setToDoList([toDo, ...toDoList]);
+      setToDo('');
+    }
+  };
+
+  return (
+    <>
+      <h1 className={styles.title}>To-Do List ({toDoList.length})</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your To-Do..."
+        />
+        <Button text={'Add To-Do'} />
+      </form>
+
+      <hr />
+
+      <ul>
+        {toDoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ToDoList;
