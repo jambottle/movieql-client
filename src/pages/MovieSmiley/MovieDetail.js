826ebe2426cfa406e1dcb40c58e6c50b326820fe
jambottle <jaewon.walks@gmail.com>
2022-07-18 23:35:27 +0900

    feat: Add MovieDetail component using useParams hook and Fetch API

---
+++ src/pages/MovieSmiley/MovieDetail.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import MovieItem from '../../components/MovieSmiley/MovieItem';
+
+function MovieDetail() {
+  const { id } = useParams();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieDetail, setMovieDetail] = useState({});
+
+  const getMovieDetail = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+    const { data } = await response.json();
+
+    setMovieDetail(data.movie);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovieDetail();
+  }, []);
+
+  return (
+    <>
+      <h1>Detail Page</h1>
+      {isLoading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <MovieItem movie={movieDetail} />
+      )}
+    </>
+  );
+}
+
+export default MovieDetail;
