8f6dd930d48807c02e3bed52128f4f23fa1bfb80
jambottle <jaewon.walks@gmail.com>
2022-07-19 23:50:13 +0900

    chore: Set base URL to route the CRA project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MovieList from './pages/MovieSmiley/MovieList';
 import MovieDetail from './pages/MovieSmiley/MovieDetail';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<MovieList />} />
         <Route exact path="/movie/:id" element={<MovieDetail />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
