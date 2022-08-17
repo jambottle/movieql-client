3bd4e2f67308ec941c4ae3ef0099919a89dfb80d
jambottle <jaewon.walks@gmail.com>
2022-08-18 00:40:21 +0900

    design: Style MovieItem.js with semantic tags

--- src/components/MovieSmiley/MovieItem.js
+++ src/components/MovieSmiley/MovieItem.js
@@ -1,21 +1,30 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import styles from './MovieItem.module.css';
+
 function MovieItem({ movie }) {
-  const { id, title, title_long, genres, summary, medium_cover_image } = movie;
+  const { id, title, year, genres, summary, medium_cover_image } = movie;
 
   return (
-    <article>
-      <img alt={title} src={medium_cover_image} />
-      <h2>
-        <Link to={`/movie/${id}`}>{title_long}</Link>
-      </h2>
-      <p>{summary.length > 240 ? `${summary.slice(0, 240)}...` : summary}</p>
-      <ul>
-        {genres.map((genre, index) => (
-          <li key={index}>{genre}</li>
-        ))}
-      </ul>
+    <article className={styles.movie}>
+      <img
+        alt={title}
+        src={medium_cover_image}
+        className={styles.movie__image}
+      />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 240 ? `${summary.slice(0, 240)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+      </div>
     </article>
   );
 }
