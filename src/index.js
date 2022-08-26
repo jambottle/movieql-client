a11a6e66a6fc3dd33d8e1f9a5d86373e10a41070
jambottle <jaewon.walks@gmail.com>
2022-08-26 23:50:58 +0900

    feat: Initialize an Apollo Client instance to access the GraphQL API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import client from './client';
 
 import App from './App';
 import './styles.css';
@@ -7,6 +9,8 @@ import './styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
